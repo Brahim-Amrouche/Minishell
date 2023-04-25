329180ef4d7ad5c6d2f7a1ecc8ccc6cd6fd4ce4f
Almanfi <nassimed.dev@gmail.com>
2023-04-25 20:47:38 +0100

    widcard working

--- wildcard.c
+++ wildcard.c
@@ -1,39 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   wildcard.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maboulkh <maboulkh@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/25 12:57:10 by maboulkh          #+#    #+#             */
+/*   Updated: 2023/04/25 19:45:38 by maboulkh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
 #include <dirent.h>
-#include <stdio.h>
-#include <string.h>
 
+char	*terminate_curr__get_next_input(char *input)
+{
+	if (!input)
+		return (NULL);
+	while (*input && *input != '*')
+		input++;
+	if (*input == '*')
+	{
+		*(input++) = '\0';
+		while (*input == '*')
+			input++;
+		if (*input)
+			return (input);
+		return (NULL);
+	}
+	return (NULL);
+}
 
-static int get_wildcard(char *str, char *input)
+static int get_wildcard(char *str, char *in)
 {
 	int i;
 	int len;
-	int offset;
+	char *match;
+	char *next_in;
+	char *input;
 
-	if (!str || !input)
+	if (!str || !in)
 		return (0);
-	i = 0;
-	while (*(input + i) && *(str + i) == *(input + i) && *(input + i) != '*')
-		i++;
-	if (*(input + i) != '*')
+	input = ft_strdup(in);
+	if (!ft_strncmp(str, "..", -1))
 		return (0);
-	input++;
-	len = strlen(str);
-	if (i > strlen(str) - strlen(input + i))
+	if (*str == '.' && *in != '.')
 		return (0);
-	offset = len - strlen(input);
-	while (*(input + i) && *(str + offset + i) == *(input + i))
-		i++;
-	if (offset + i != len)
+	if (*input != '*' && *input != *str)
+		return 0;
+    if (input[ft_strlen(input) - 1] != '*'
+        && input[ft_strlen(input) - 1] != str[ft_strlen(str) - 1])
+        return 0;
+	if (ft_strchr(input, '*') == NULL)
 		return (0);
-	return (1);
+	// if (!*input)
+	// 	return (0);
+	while (*input == '*')
+		input++;
+	while (*str)
+	{
+		if (!*input)
+			return (1);
+		next_in = terminate_curr__get_next_input(input);
+		match = ft_strnstr(str, input, -1);
+		if (!match)
+				return (0);
+		if (!next_in)
+			return (1);
+		str = match + ft_strlen(input);
+		input = next_in;
+	}
+	return (0);
 }
 
 int main(int argc, char *argv[], char **envp)
 {
-	DIR *dir;
-	struct dirent *direntf;
+	DIR		*dir;
+	struct	dirent *direntf;
 
-	dir = opendir("/workspaces/Minishell");
+	dir = opendir("/Users/maboulkh/Desktop/Minishell");
 	while (1)
 	{
 		direntf = readdir(dir);
