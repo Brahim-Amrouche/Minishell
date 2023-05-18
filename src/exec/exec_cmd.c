f70ebc6d0572508f499a14f3c16b77ace1464c24
Almanfi <nassimed.dev@gmail.com>
2023-05-18 15:04:15 +0100

    not really a printed tree :(

--- src/exec/exec_cmd.c
+++ src/exec/exec_cmd.c
@@ -6,7 +6,7 @@
 /*   By: maboulkh <maboulkh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/01 19:57:04 by maboulkh          #+#    #+#             */
-/*   Updated: 2023/05/17 20:35:07 by maboulkh         ###   ########.fr       */
+/*   Updated: 2023/05/18 15:03:47 by maboulkh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,37 +29,6 @@ char **get_env_var(char *name, char **env)
 	return (NULL);
 }
 
-// int calc_tree_size(char **tree)
-// {
-// 	int	size;
-
-// 	if (!tree)
-// 		return (0);
-// 	size = 0;
-// 	while (tree[size])
-// 		size++;
-// 	return (size);
-// }
-
-// char **tree_d add_element_to_tree(char **tree, char *elem)
-// {
-// 	char	**res;
-// 	int		size;
-// 	int		i;
-
-// 	size = calc_tree_size(tree);
-// 	res = malloc((size + 1) * sizeof(char *));
-// 	i = 0;
-// 	while (i < size)
-// 	{
-// 		res[i] = tree[i];
-// 		++i;
-// 	}
-// 	res[i] = elem;
-// 	free(tree);
-// 	return (res);
-// }
-
 int pow2(int n)
 {
 	int res;
@@ -79,24 +48,24 @@ void traverse_tree(t_exec_tree *tree, int depth, int offset, char **tree_d, int 
 		node = tree->info.exec_node;
 		if (depth > *tree_size)
 			*tree_size = depth;
-		tree_d[pow2(depth) + offset] = node->cmd[0];
+		tree_d[pow2(depth) + offset - 1] = node->cmd[0];
 		// printf("\t|%s|%d\t", node->cmd[0], depth);
 		if (!ft_strncmp("exit", node->cmd[0], -1))
 			exit(0);
 	}
 	else
 	{
-		tree_d[pow2(depth) + offset] = types[tree->type];
+		tree_d[pow2(depth) + offset - 1] = types[tree->type];
 	}
 	if (tree->left)
 	{
 		// printf("left\t");
-		traverse_tree(tree->left, depth + 1, offset * 2, tree_d, tree_size, types);
+		traverse_tree(tree->left, depth + 1, 2 * offset, tree_d, tree_size, types);
 	}
 	if (tree->right)
 	{
 		// printf("right\t");
-		traverse_tree(tree->right, depth + 1, offset + 1 + (2 * offset), tree_d, tree_size, types);
+		traverse_tree(tree->right, depth + 1, 2 * offset + 1, tree_d, tree_size, types);
 
 		printf("\n");
 	}
@@ -105,27 +74,34 @@ void traverse_tree(t_exec_tree *tree, int depth, int offset, char **tree_d, int 
  void print_tree_n(char ** tree, int depth)
  {
 	int i;
+	int j;
 	int size;
 	int d;
 
 	d = 0;
+	size = 1;
 	while (d < depth + 1)
 	{
-		size = pow2(d + 1) - 1;
-		i = 0;
-		while (i < (pow2(depth - d) / 2))
-		{
-			printf("%5s\t", ".");
-			i++;
-		}
+		if (d % 2)
+			printf("    ");
 		i = pow2(d) - 1;
 		while (i < size)
 		{
-			printf("%5s\t", tree[i]);
+			j = 0;
+			while (j < (pow2(depth) / pow2(d + 1)))
+			{
+				printf("        ");
+				j++;
+			}
+			if (tree[i])
+				printf("%8s,%d", tree[i], i);
+			else
+				printf("        ");
 			i++;
 		}
 		printf("\n");
 		d++;
+		size = pow2(d + 1) - 1;
 	}
  }
 
@@ -151,17 +127,6 @@ int	exec_cmd(t_minishell *minishell)
 	types = ft_split("O | && || x", ' ');
 	traverse_tree(tree, 0, 0, draw, &tree_size, types);
 	print_tree_n(draw, tree_size);
-	// print_tree(tree);
-
-	// if (tree->type == LOGICAL_EXEC)
-	// {
-	// 	node = tree->info.exec_node;
-	// 	printf("cmd -> |%s|\n", node->cmd[0]);
-	// 	if (!ft_strncmp("exit", node->cmd[0], -1))
-	// 		exit(0);
-	// }
-	// else
-		// printf("none\n");
 	return (0);
 
 	// t_exec_node **oper_cmds = minishell->parsed_cmds.op_cmds;
